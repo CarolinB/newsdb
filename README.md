464db7ea92dc88aa4f9925bc14ae3c8b7e8fd8f4
Carolin Bruederle <rumpf@team23.de>
2018-12-21 11:32:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+#About newsdb.py
+
+It is a simple program designed for magazines or newspapers to print the output on  the 3 following questions to the console:
+1. What are the most popular three articles of all time?
+2. Who are the most popular article authors of all time?
+3. On which days did more than 1% of all requests lead to errors?
+
+The program is written in Python, using a relational DB and PostgreSQL.
+
+#Installation/Environment
+
+Requires Python 2.7 and PostgreSQL version 9.5.x.
+
+#Output example
+```
+Most popular 3 articles of all time:
+
+"Candidate is jerk, alleges rival" - 342102 views
+"Bears love berries, alleges bear" - 256365 views
+"Bad things gone, say good people" - 171762 views
+
+Most popular article authors of all time:
+
+Ursula La Multa" - 512805
+Rudolf von Treppenwitz" - 427781
+Anonymous Contributor" - 171762
+Markoff Chaney" - 85387
+
+Days with more than 1% of all requests leading to errors:
+
+Jul 17, 2016 - 2.26
+```
+
+#Licence
+No licence required.
---
+++ newsdb.py
@@ -0,0 +1,71 @@
+"""
+Database Code for the News Database:
+3 functions that print the answers to the following questions -
+1) Return the three articles with the most views
+2) Return the authors whose articles got the most views
+3) Return the days when more than 1 percent of all requests led to errors
+"""
+
+
+import psycopg2
+from datetime import datetime
+
+DBNAME = "news"
+
+
+def get_3_most_popular_articles():
+    """Return the three articles with the most views"""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("SELECT a.title, COUNT(a.id) AS count FROM articles a JOIN log l ON (l.path ILIKE '%' || a.slug || '%') GROUP BY a.id ORDER BY count DESC LIMIT 3")
+    most_popular_3articles = c.fetchall()
+    db.close()
+    print("Most popular 3 articles of all time: \n")
+    x = 0
+    while x < len(most_popular_3articles):
+        print("\"" + str(most_popular_3articles[x][0]) + "\"" + " - " + str(most_popular_3articles[x][1]) + " views")
+        x = x+1
+
+
+def get_authors_with_most_views():
+    """Return the authors whose articles got the most views"""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("SELECT auth.name, COUNT(auth.id) AS views FROM authors auth JOIN articles a ON (a.author = auth.id) JOIN log l ON (l.path ILIKE '%' || a.slug || '%') GROUP BY auth.id ORDER BY views DESC;")
+    authors_most_views = c.fetchall()
+    db.close()
+    print("\nMost popular article authors of all time: \n")
+    x = 0
+    while x < len(authors_most_views):
+        print(str(authors_most_views[x][0]) + "\"" + " - " + str(authors_most_views[x][1]))
+        x = x+1
+
+
+def convert_date(datevalue):
+        strDate = str(datevalue)
+        objDate = datetime.strptime(strDate, '%Y-%m-%d')
+        format_date = datetime.strftime(objDate, '%b %d, %Y')
+        return format_date
+
+
+def get_days_errors_higher_1percent():
+    """Return the days when more than 1% of all requests led to errors"""
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute("SELECT day, errorfraction * 100 AS percentage FROM (SELECT time::DATE AS day, (COUNT(CASE WHEN status <> '200 OK' THEN 1 END)::decimal) / (COUNT(*)::decimal) as errorfraction FROM log GROUP BY day) AS errorsperday WHERE errorfraction > 0.01;")
+    errors_higher_1percent = c.fetchall()
+    db.close()
+    print("\nDays with more than 1% of all requests leading to errors: \n")
+
+    x = 0
+    while x < len(errors_higher_1percent):
+        format_date = convert_date(errors_higher_1percent[x][0])
+        percent = round(errors_higher_1percent[x][1], 2)
+        print(str(format_date) + " - " + str(percent))
+        x = x+1
+
+
+get_3_most_popular_articles()
+get_authors_with_most_views()
+get_days_errors_higher_1percent()
+
